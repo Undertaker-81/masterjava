b35fb82542e4973ed927b59a4ce9e2f0fb07b014
panfilov.d <Undertaker-81@mail.ru>
2021-02-10 08:36:47 +0500

    Revert "hw05 - DB init, add new tables"
    This reverts commit e7e8d39f

--- config_templates/sql/initDB.sql
+++ config_templates/sql/initDB.sql
@@ -3,7 +3,6 @@ DROP SEQUENCE IF EXISTS user_seq;
 DROP TYPE IF EXISTS user_flag;
 
 CREATE TYPE user_flag AS ENUM ('active', 'deleted', 'superuser');
-CREATE TYPE group_status AS ENUM ('REGISTERING', 'CURRENT', 'FINISHED');
 
 CREATE SEQUENCE user_seq START 100000;
 
@@ -11,34 +10,7 @@ CREATE TABLE users (
   id        INTEGER PRIMARY KEY DEFAULT nextval('user_seq'),
   full_name TEXT NOT NULL,
   email     TEXT NOT NULL,
-  flag      user_flag NOT NULL,
-  town_id   INTEGER NOT NULL,
-  FOREIGN KEY (town_id) REFERENCES towns (id)
+  flag      user_flag NOT NULL
 );
 
 CREATE UNIQUE INDEX email_idx ON users (email);
-
-CREATE TABLE towns (
-                       id        INTEGER PRIMARY KEY DEFAULT nextval('user_seq'),
-                       full_name TEXT NOT NULL,
-                       shot_name TEXT NOT NULL
-);
-
-CREATE TABLE groups (
-                       id        INTEGER PRIMARY KEY DEFAULT nextval('user_seq'),
-                       name         TEXT NOT NULL,
-                       status     group_status NOT NULL,
-                       project_id INTEGER,
-                       FOREIGN KEY (project_id) REFERENCES projects(id)
-);
-
-CREATE TABLE projects (
-                       id        INTEGER PRIMARY KEY DEFAULT nextval('user_seq'),
-                       name TEXT NOT NULL
-);
-CREATE TABLE users_groups (
-                          user_id        INTEGER NOT NULL ,
-                          group_id      INTEGER NOT NULL,
-                          FOREIGN KEY (user_id) REFERENCES users (id),
-                          FOREIGN KEY (group_id) REFERENCES groups (id)
-);
