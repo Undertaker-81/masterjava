7f29bd903ba174071e547cce901a514d297f8f0f
Dmitriy Panfilov <undertaker-81@mail.ru>
2021-02-28 18:37:03 +0500

    fix

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -5,7 +5,7 @@ import com.google.common.collect.ImmutableSet;
 public class MailWSClientMain {
     public static void main(String[] args) {
         MailWSClient.sendToGroup(
-                ImmutableSet.of(new Addressee("To <undertaker-81@mail.ru.ru>")),
+                ImmutableSet.of(new Addressee("To <undertaker-81@mail.ru>")),
                 ImmutableSet.of(new Addressee("Copy <undertaker-81@mail.ru>")), "Subject", "Body");
     }
 }
